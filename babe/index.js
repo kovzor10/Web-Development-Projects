9f304f82815fb68e89021557d6a12ddc354f9bfe
kovzor10 <kovzor16@gmail.com>
2024-07-16 21:15:14 +0300

    babe commit

--- babe/index.js
+++ babe/index.js
@@ -5,29 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const mainDiv = document.getElementById( 'main' );
 
     firstButton.addEventListener( 'click', () => {
-
         firstDiv.style.display = 'none';
         mainDiv.style.display = 'block';
         document.body.style.backgroundColor = '101010';
+    }) 
 
-    })
-    
 });
 
-    const tablinks = document.getElementsByClassName( "tab-links" );
-    const tabcontents = document.getElementsByClassName( "tab-contents" );
+const tablinks = document.getElementsByClassName( "tab-links" );
+const tabcontents = document.getElementsByClassName( "tab-contents" );
 
-    function opentab( tabname ){
-
-        for( tablink of tablinks ){
-             tablink.classList.remove( "active-link" );
-        }
-
-        for( tabcontent of tabcontents ){
-            tabcontent.classList.remove( "active-tab" );
-        }
-
-        event.currentTarget.classList.add( "active-link" );
-        document.getElementById( tabname ).classList.add( "active-tab" );
-
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
