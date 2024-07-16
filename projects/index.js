e088a2190078c813bb42a75074694ca73ef3cf10
kovzor10 <kovzor16@gmail.com>
2024-07-16 18:16:50 +0300

    babe_page commit

---
+++ projects/index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const firstButton = document.getElementById( 'first-button' );
+    const firstDiv = document.getElementById( 'first' );
+    const mainDiv = document.getElementById( 'main' );
+
+    firstButton.addEventListener( 'click', () => {
+
+        firstDiv.style.display = 'none';
+        mainDiv.style.display = 'block';
+        document.body.style.backgroundColor = '101010';
+
+    })
+    
+});
+
+    const tablinks = document.getElementsByClassName( "tab-links" );
+    const tabcontents = document.getElementsByClassName( "tab-contents" );
+
+    function opentab( tabname ){
+
+        for( tablink of tablinks ){
+             tablink.classList.remove( "active-link" );
+        }
+
+        for( tabcontent of tabcontents ){
+            tabcontent.classList.remove( "active-tab" );
+        }
+
+        event.currentTarget.classList.add( "active-link" );
+        document.getElementById( tabname ).classList.add( "active-tab" );
+
+    }
